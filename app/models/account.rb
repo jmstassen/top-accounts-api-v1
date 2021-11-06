963bf1e49e362ef97f5ccdeb1c00f87b2fdcb7c6
John M. Stassen <john.stassen@gmail.com>
2021-11-06 13:04:43 -0700

    add validation on account_name

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,8 @@
 class Account < ApplicationRecord
-    has_many :activities
-    has_many :person_accounts
-    has_many :people, through: :person_accounts
+  has_many :activities
+  has_many :person_accounts
+  has_many :people, through: :person_accounts
+
+  validates :account_name, presence: true
+
 end
