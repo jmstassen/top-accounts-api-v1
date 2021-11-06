250834ec65a68664e409aeea5b782109383b4a2b
John M. Stassen <john.stassen@gmail.com>
2021-11-06 16:27:39 -0700

    add validation for activity_type

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,8 +2,6 @@ class Activity < ApplicationRecord
     belongs_to :account
     has_many :person_activities
     has_many :people, through: :person_activities
-
     validates :activity_type, presence: true
-    # validates_inclusion_of :activity_type, :in => ['meeting', 'email', 'summary', 'objective']
-
+    validates_inclusion_of :activity_type, :in => ['meeting', 'email', 'event', 'summary', 'objective', 'financial', 'document']
 end
