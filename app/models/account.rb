981611772fb615f3eeadc02d57645eb945a345f7
John M. Stassen <john.stassen@gmail.com>
2021-11-06 16:27:16 -0700

    clean up format

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,5 @@ class Account < ApplicationRecord
   has_many :activities
   has_many :person_accounts
   has_many :people, through: :person_accounts
-
   validates :account_name, presence: true
-
 end
