c925a199ab174f5fe62a7bf12da42d673fd21593
John M. Stassen <john.stassen@gmail.com>
2021-11-06 16:26:53 -0700

    build out methods

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,28 +1,36 @@
-    class Api::V1::AccountsController < ApplicationController
-      require_relative '../../../serializers/account_serializer'
+class Api::V1::AccountsController < ApplicationController
+  require_relative '../../../serializers/account_serializer'
 
-      def create
-        account = Account.create(account_params)
-        render json: account
-      end
+  def index
+    accounts = Account.all
+    render json: accounts
+  end
 
-      def update
-      end
-
-      def index
-        accounts = Account.all
-        render json: accounts
-      end
-
-      def show
-      end
-
-      def destroy
-      end
-
-      private
-
-      def account_params
-        params.require(:account).permit(:account_name, :id)
-      end
+  def create
+    account = Account.new(account_params)
+    if account.save
+      render json: account
+    else
+      render json: {error: 'Could not create account'}
     end
+  end
+
+  def show
+    account = Account.find(params[:id])
+    render json: account
+  end
+
+  def update
+  end
+
+  def destroy
+    account = Account.find(params[:id])
+    account.destroy
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:account_name, :id)
+  end
+end
