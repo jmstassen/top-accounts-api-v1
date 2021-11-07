b259586d41d97120ec36c98f77f3fc1e4f747737
John M. Stassen <john.stassen@gmail.com>
2021-11-07 09:38:02 -0800

    set serializer attributes for activity

--- app/serializers/activity_serializer.rb
+++ app/serializers/activity_serializer.rb
@@ -3,7 +3,7 @@ class ActivitySerializer < ActiveModel::Serializer
   # has_many :person_activities
   # has_many :people, through: :person_activities
 
-  attributes :id, :activity_type, :activity_date, :activity_name, :created_at, :people
+  attributes :id, :activity_type, :activity_date, :activity_name, :created_at
 
   # def people
   #   ActiveModelSerializers::SerializableResource.new(object.people, each_serializer: PersonSerializer)
