88f746fcbbb4999d4cc2f6abafb80be08331e63d
John M. Stassen <john.stassen@gmail.com>
2021-11-11 17:31:18 -0800

    adding dependent: :destroy for activities

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,5 @@
 class Account < ApplicationRecord
-  has_many :activities
+  has_many :activities, dependent: :destroy
   has_many :person_accounts
   has_many :people, through: :person_accounts
   validates :account_name, presence: true
