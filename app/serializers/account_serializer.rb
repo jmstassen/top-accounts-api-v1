ae8f12e4d3384fd1bc27c32a427c0ac8b21e07cd
John M. Stassen <john.stassen@gmail.com>
2021-11-11 17:32:05 -0800

    ordering activity by activity date descending

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,6 +1,8 @@
 class AccountSerializer < ActiveModel::Serializer
   attributes :account_name, :id
-  has_many :activities
+  has_many :activities do
+    object.activities.order(activity_date: :desc)
+  end
   # has_many :person_accounts
   has_many :people, through: :person_accounts, serializer: PersonSerializer
 
