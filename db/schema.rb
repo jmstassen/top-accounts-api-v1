e2dacaa6776e3ae8a8a3f429865944b23363e99a
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:17:19 -0800

    update scheme to final db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_20_031112) do
+ActiveRecord::Schema.define(version: 2021_10_20_030514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,36 +31,5 @@ ActiveRecord::Schema.define(version: 2021_10_20_031112) do
     t.index ["account_id"], name: "index_activities_on_account_id"
   end
 
-  create_table "people", force: :cascade do |t|
-    t.string "person_name"
-    t.string "person_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "person_accounts", force: :cascade do |t|
-    t.bigint "person_id", null: false
-    t.bigint "account_id", null: false
-    t.integer "person_account_role"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["account_id"], name: "index_person_accounts_on_account_id"
-    t.index ["person_id"], name: "index_person_accounts_on_person_id"
-  end
-
-  create_table "person_activities", force: :cascade do |t|
-    t.bigint "person_id", null: false
-    t.bigint "activity_id", null: false
-    t.integer "person_activity_role"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["activity_id"], name: "index_person_activities_on_activity_id"
-    t.index ["person_id"], name: "index_person_activities_on_person_id"
-  end
-
   add_foreign_key "activities", "accounts"
-  add_foreign_key "person_accounts", "accounts"
-  add_foreign_key "person_accounts", "people"
-  add_foreign_key "person_activities", "activities"
-  add_foreign_key "person_activities", "people"
 end
