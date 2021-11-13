ba5bc96a6c50ef22463ab4d8a9b751b5ee1146dc
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:16:34 -0800

    remove reference to people

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,7 +1,5 @@
 class Activity < ApplicationRecord
     belongs_to :account
-    has_many :person_activities
-    has_many :people, through: :person_activities
     validates :activity_type, presence: true
     validates_inclusion_of :activity_type, :in => ['meeting', 'email', 'event', 'summary', 'objective', 'financial', 'document']
 
