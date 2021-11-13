0894c6748e54b6c66e7f5bb785a888837008e798
John M. Stassen <john.stassen@gmail.com>
2021-11-12 20:16:55 -0800

    remove reference to people

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,13 +1,7 @@
 class AccountSerializer < ActiveModel::Serializer
   attributes :account_name, :id
+  
   has_many :activities do
     object.activities.order(activity_date: :desc)
   end
-  # has_many :person_accounts
-  has_many :people, through: :person_accounts, serializer: PersonSerializer
-
-  # def activities
-  #   ActiveModelSerializers::SerializableResource.new(object.activities, each_serializer: ActivitySerializer)
-  # end
-
 end
